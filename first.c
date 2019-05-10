2a5d899217999d47f6c77ab0c005d57a0fa4b389
unknown <jjanardhan@gmail.com>
2019-05-10 16:59:42 +0530

    added

--- first.c
+++ first.c
@@ -1,7 +1,23 @@
 #include<stdio.h>
 
+int test = 10;
+
+int testing[] = {10, 20, 30, 40};
+
+int x = sizeof(testing)/sizeof(testing[0]);
+int s;
+
 int main(){
 
-printf("this is test");
+    printf("%d\n", test);
+    printf("%d\n", x );
+    printf("this is test");
+    printf("enter value: ");
+    scanf("%d", &s);
+    printf("entered value is %d: ", s);
     return 0;
 }
+
+
+
+
--- readme.txt
+++ readme.txt
@@ -1,1 +1,8 @@
-first
+int testing[] = {10, 20, 30, 40};
+
+int x = sizeof(testing)/sizeof(testing[0]);
+int s;
+
+
+scanf("%d", &s);
+    printf("entered value is %d: ", s);

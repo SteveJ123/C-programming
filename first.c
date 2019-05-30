290a05e98215db212cf4555e066374ba21024fcb
unknown <jjanardhan@gmail.com>
2019-05-31 00:04:44 +0530

    added

--- first.c
+++ first.c
@@ -1,20 +1,12 @@
 #include<stdio.h>
 
-int test = 10;
 
-int testing[] = {10, 20, 30, 40};
+int test1 = 12;
 
-int x = sizeof(testing)/sizeof(testing[0]);
-int s;
 
 int main(){
-
-    printf("%d\n", test);
-    printf("%d\n", x );
-    printf("this is test");
-    printf("enter value: ");
-    scanf("%d", &s);
-    printf("entered value is %d: ", s);
+    
+    printf("long int is: %d ", test1);
     return 0;
 }
 
--- readme.txt
+++ readme.txt
@@ -1,8 +1,24 @@
+gcc -g first.c -o first.exe
+
+
+
+
+what preprocessor does
+gcc -E first.c
+
+
+
+what assemble code
+gcc -S first.c
+
+
+
 int testing[] = {10, 20, 30, 40};
 
 int x = sizeof(testing)/sizeof(testing[0]);
 int s;
-
+int x = sizeof(testing)/sizeof(testing[0]);
 
 scanf("%d", &s);
     printf("entered value is %d: ", s);
+added now again test
